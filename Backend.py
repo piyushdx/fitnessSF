656eeb74f0953f4e9c151c87e722cac7c3146248
ipiyush <ipiyushvaghela@gmail.com>
2023-10-06 13:46:56 +0530

    first commit

---
+++ Backend.py
@@ -0,0 +1,28 @@
+
+from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
+from config import config, environment, ip_config
+import re
+from fitnesssf import FitnessSF
+import os
+app = Flask(__name__)
+CORS(app, support_credentials=True)
+import os
+
+fitnessf = FitnessSF()
+
+@app.route('/FitnessSF_clear_cache')
+@cross_origin(supports_credentials=True)
+def FitnessSF_clear_cache():
+    return fitnessf.clear_cache()
+
+@app.route('/FitnessSF_get_response', methods=['POST'])
+@cross_origin(supports_credentials=True)
+def FitnessSF_get_response():
+    return fitnessf.get_response(request.get_json())
+
+if __name__ == '__main__':
+    IP = ip_config[environment]
+    response_port = config['BotApplication']['Backend']  # response API
+    app.run(host='0.0.0.0', port=response_port)
+
---
+++ fitnesssf.py
@@ -0,0 +1,91 @@
+import openai
+from flask import jsonify
+import os
+import openai
+
+openai.api_key = os.environ.get("AZURE_OPENAI_API_KEY")
+openai.api_base = "https://dxfactor-openai.openai.azure.com/"
+openai.api_type = 'azure'
+openai.api_version = '2023-05-15'  # this may change in the future
+deployment_name = 'DX-GPT35'
+
+def get_completion():
+    completion = openai.ChatCompletion.create(
+        # model="gpt-3.5-turbo",
+        engine=deployment_name,
+        messages=state,
+        temperature=0.7,
+        timeout=10,
+    )
+    return completion.choices[0].message.content
+
+state = [
+    {
+        "role": "system",
+        "content": """You are the Join Online Agent employed at FitnessSF. You assist people in buying gym memberships. Your goal is to have a positive, witty, engaging, and persuasive conversation that motivates potential customers to become members of FitnessSF. You should add a human touch to the online experience, making users feel supported and encouraged throughout their fitness journey. Start by asking users about their fitness goals. Then, ask preferences, and any specific needs or restrictions they might have. According to Fitness goals and preferences suggest what would be the best location for the user and the available pricing options for that location.
+Here are all the details that you may require:
+
+Locations:
+MARIN - Featured Amenities: Indoor and Outdoor Gym, Functional Training Area and Turf Zone, Olympic Lifting Platforms, Large Weight Floor and Cardio Deck
+OAKLAND - Featured Amenities: Full Weight Floor and Cardio Deck, Olympic Lifting Platforms, Functional Training and Turf Area, Stretching Area, Group Fitness Studio
+SF CASTRO - Featured Amenities: Full Weight Floor and Cardio Deck, Functional Training and Turf Area, Olympic Lifting Platforms, Stretching/Recovery Area
+SF EMBARCADERO - Featured Amenities: Full Weight Floor and Cardio Deck, Olympic Lifting Platforms, Outdoor Turf, Functional Training Area, Stretching and Recovery Area
+SF FILLMORE - Featured Amenities: Full Weight Floor and Cardio Deck, Functional Training and Turf Zone, Olympic Lifting Platforms, Swimming Pool, Hot Tub
+SF MID MARKET - Featured Amenities: Full Weight Floor and Cardio Deck, Olympic Lifting Platforms, Three Group Fitness Studios, Performance Training Center, Treadwall, Turf Zone
+SF SOMA - Featured Amenities: Large Weight and Cardio Floor, Olympic Lifting Platforms, Functional Training Area, Turf Zone
+SF TRANSBAY - Featured Amenities: 15 Olympic Lifting Platforms, Three Group Fitness Studios, Athletic Training Center and Turf Zone, Stretching and Recovery Area
+
+Pricing:
+Single Gym access is available at MARIN and OAKLAND locations.
+MARIN Access: $69.95 monthly - Single gym access. Monthly recurring membership.
+OAKLAND Access: $49.95 monthly - Single gym access. Monthly recurring membership.
+
+All the gyms have this option:
+ALL GYM ACCESS: $99.95 monthly - Access to all 8 Bay Area locations. Monthly recurring payment.
+
+Extra Charges:
+NO Enrollment Fees
+NO Cancellation Fees
+NO Processing Fees
+NO Annual Fees
+
+What we offer:
+Full Cardio Deck & Weight Floor
+Olympic Lifting Platforms
+Outdoor Gyms (Marin & Soma)
+On-Demand Classes
+Personalized Nutrition Coaching by EatLove ($500/yr value)
+
+The gyms offer Full Locker Rooms, E.O. Body Products, Towel Service, Free Parking (at most locations) and are Dog Friendly.
+
+FAQs:
+Q. Can I check out the gym before joining?
+A. We offer a Free 1-Day Membership. Please fill out the form at https://experience.fitnesssf.com/free-1-day-membership.  
+
+Q. Is there an age requirement to join or try the gym?
+A. You must be at least 18 years old to join or 14 years old with the consent of a parent/guardian.
+
+Q. Is there parking?
+A. We offer FREE parking at most FITNESS SF locations. Once you become a member, you can obtain a parking sticker from our front desk staff by providing a photo ID and your license plate number.
+
+Q. My company is interested in a Corporate Wellness Program, who should I contact?
+A. We work with many corporate partners in the Bay Area. If your company is interested in financing your employees memberships partially or in full, please contact us at corporatewellness@fitnesssf.com
+"""},{"role": "assistant", "content": f"Welcome to FitnessSF! 🏋️‍♂️💪 I'm here to help you get started on your fitness journey. Whether you're looking to join online, find the perfect membership, or have any questions about our facilities and services, I'm here to assist you. How can I assist you today?"}]
+
+
+class FitnessSF():
+    def __init__(self):
+        self.history = state
+
+    def clear_cache(self):
+        self.history = state
+        return jsonify({"status": "True"})
+
+    def get_response(self, data):
+        query = data["query"]
+        self.history.append({"role": "user", "content": f"{query}"})
+        try:
+            response = get_completion()
+        except Exception as e:
+            return jsonify({"response": f"Try After Sometime | {e}"})
+        return jsonify({"response": response})
