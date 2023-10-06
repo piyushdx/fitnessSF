d417fb2226166a957ed5b43a16e58fcb415a3901
ipiyush <ipiyushvaghela@gmail.com>
2023-10-06 13:56:38 +0530

    updated the config file and ports

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ ip_config = {'server': "18.144.92.141", # this is server URL
              'local': "127.0.0.1"}
 
 config = {"BotApplication": {
-    "Backend": 5001,   # port needs to be public - 8001
-    "Frontend": 5002,  # port needs to be public - 8002
+    "Backend": 5003,   # port needs to be public - 8001
+    "Frontend": 5004,  # port needs to be public - 8002
     }
 }
