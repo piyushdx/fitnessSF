3741e2bbfa99d31c92a6854b25b61eaecaf99b2a
Pankti <pankti@dxfactor.com>
2023-10-11 19:03:06 +0530

    added_csp

--- Frontend.py
+++ Frontend.py
@@ -3,6 +3,7 @@ import os
 import threading
 import re
 from config import config, environment, ip_config
+from flask_csp import csp
 from flask_cors import CORS, cross_origin
 
 # ===================================================================================================
@@ -13,8 +14,8 @@ with open("ChatBotUI/static/js/script_base.js", "r", encoding='utf-8') as f:
     js_code= f.read()
 
 js_code = re.sub('"http://127.0.0.1:1563/"', f'"http://{IP}:{response_port}/"', js_code)
-print("js_code*********")
-print(js_code)
+# print("js_code*********")
+# print(js_code)
 with open("ChatBotUI/static/js/script.js", "w") as f:
     f.write(js_code)
     f.close()
@@ -35,7 +36,15 @@ CORS(app, resources={r"/fitnesssf": {"origins": "http://localhost:3000"}})
 #     # Add more directives as needed
 # }
 # talisman = Talisman(app, content_security_policy=csp)
+app.config['CSP'] = {
+    'default-src': "'self'",
+    'script-src': ["'self'", "'unsafe-inline'", "'unsafe-eval'", "/static/"],
+    'style-src': ["'self'", "'unsafe-inline'", "/static/"],
+    'font-src': ["'self'", 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css', '/static/'],
+}
 
+
+# csp(app)
 
 @app.route('/')
 @cross_origin(supports_credentials=True)
